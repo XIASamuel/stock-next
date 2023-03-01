4f3c8472d6302b2934db99ac008d9c1ee88391ce
XIASamuel <u6328003@au.edu>
2023-03-01 15:43:58 +0700

    change

--- pages/blogs/index.jsx
+++ pages/blogs/index.jsx
@@ -55,7 +55,7 @@ export default function Home({ blogs }) {
   )
 }
 export async function getServerSideProps() {
-  const res = await fetch(`https://stock-next-chi.vercel.app/api/blogs/articles/`)
+  const res = await fetch(`http://localhost:3000/api/blogs/articles/`)
   const blogs = await res.json()
   // console.debug('blog 1', blogs)
   return { props: { blogs } }
